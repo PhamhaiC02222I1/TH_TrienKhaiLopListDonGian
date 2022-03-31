4b4416c8696238d8502fce1c06e739285a2b75da
hai <humg.ola@gmail.com>
2022-03-31 14:31:26 +0700

    up

---
+++ src/com/company/MyListTest.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import com.sun.deploy.cache.CacheEntry;
+
+public class MyListTest {
+    public static void main(String[] args) {
+       MyList<Integer> listInterger =new MyList<Integer>();
+       listInterger.add(1);
+       listInterger.add(2);
+       listInterger.add(3);
+       listInterger.add(4);
+       listInterger.add(5);
+       listInterger.add(4);
+       listInterger.add(4);
+       listInterger.add(4);
+       listInterger.add(4);
+       listInterger.add(4);
+       listInterger.add(9);
+       listInterger.add(6);
+
+        System.out.println("element 1: "+listInterger.get(1));
+        System.out.println("element 2: "+listInterger.get(2));
+        System.out.println("element 4: "+listInterger.get(4));
+        System.out.println("element 10: "+listInterger.get(10));
+
+//
+
+
+    }
+}
